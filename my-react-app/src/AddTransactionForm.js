a8826c6152f4a67c3014eb109384d9bb2c06426c
Wangari Samantha <samanthanyams5@gmail.com>
2023-04-24 14:49:40 +0300

    web working

---
+++ my-react-app/src/AddTransactionForm.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Transaction from "./Transaction";
+
+function AddTransactionForm() {
+  const [date, setDate] = useState("")
+  const [description, setDescription] = useState("")
+  const [category, setCategory] = useState("")
+  const [amount, setAmount] = useState("")
+  function handleSubmit(e) {
+    e.preventDefault()
+    fetch("http://localhost:8001/transactions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({Transaction}),
+    });
+     
+  }
+  return (
+    <div className="ui segment">
+      <form onSubmit={handleSubmit} className="ui form">
+        <div className="inline fields">
+          <input className="inputField" value={date} onChange={(e) => setDate(e.target.value)} type="date" name="date" />
+          <input className="ui inverted input"  value={description} onChange={(e) => setDescription(e.target.value)} type="text" name="description" placeholder="Description" />
+          <input  value={category} onChange={(e) => setCategory(e.target.value)} type="text" name="category" placeholder="Category" />
+          <input value={amount} onChange={(e) => setAmount(e.target.value)} type="number" name="amount" placeholder="Amount" step="0.01" />
+        </div><br></br>
+        <button className="ui button" type="submit">
+          Add Transaction
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default AddTransactionForm;
--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,46 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import './App.css'
-import Header from './Header'
-import TransactionTable from './TransactionTable';
-import NewTransactionForm from './NewTransactionForm';
-import SearchBar from './SearchBar';
+import React from "react";
+import "./App.css"
+import AccountContainer from "./AccountContainer";
 
 function App() {
-  const [transactions, setTransactions] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
-
-  useEffect(() => {
-    fetch('http://localhost:8001/transactions')
-      .then(response => response.json())
-      .then(data => setTransactions(data.transactions))
-      .catch(error => console.log(error));
-  }, []);
-
-  function handleNewTransactionSubmit(formData) {
-    fetch('http://localhost:8001/transactions', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    })
-      .then(response => response.json())
-      .then(data => setTransactions([...transactions, data]))
-      .catch(error => console.log(error));
-  }
-
-  function handleSearchTermChange(event) {
-    setSearchTerm(event.target.value);
-  }
-
-  const filteredTransactions = transactions.filter(transaction => transaction.description.toLowerCase().includes(searchTerm.toLowerCase()));
-
   return (
-    <div>
-      <Header />
-      <SearchBar searchTerm="Search your Recent Transactions" onSearchTermChange={handleSearchTermChange} />
-      <NewTransactionForm onNewTransactionSubmit={handleNewTransactionSubmit} />
-      <TransactionTable transactions={filteredTransactions} />
+    <div className="ui raised segment">
+      <div >
+        <h2>The Royal Bank of Flatiron</h2>
+      </div>
+      <AccountContainer />
     </div>
   );
 }
